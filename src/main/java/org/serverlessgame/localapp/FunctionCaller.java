2e1428a1d837fd18e60776653716234e54310ff4
hirsceva <hirsceva@students.zhaw.ch>
2021-10-11 19:19:19 +0200

    Call function repeatedly get response-time

--- src/main/java/org/serverlessgame/localapp/FunctionCaller.java
+++ src/main/java/org/serverlessgame/localapp/FunctionCaller.java
@@ -4,6 +4,9 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
 import javax.ws.rs.client.ClientBuilder;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class FunctionCaller {
     public static void main(String[] args) {
@@ -11,7 +14,29 @@ public class FunctionCaller {
         ResteasyWebTarget target = client.target("https://serverlessgame-zhaw.azurewebsites.net");
 
         ApiProxy simple = target.proxy(ApiProxy.class);
-        String output = simple.getResponse("4");
-        System.out.println(output);
+
+        long start = System.currentTimeMillis();
+
+        int n = 100;
+
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        for(int i=0; i < n; i++){
+            es.execute(new Runnable() {
+                @Override
+                public void run() {
+                    String output = simple.getResponse("1");
+                    System.out.println(output);
+                }
+            });
+        }
+        es.shutdown();
+        try{
+            es.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+
+        System.out.println("time: " + (System.currentTimeMillis() - start)/n);
     }
 }
